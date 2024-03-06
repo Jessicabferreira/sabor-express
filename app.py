ad347902c5b3ecf15f14889ae338a776d04c22b3
Jessica Berteloni Ferreira <jessiberteloni@gmail.com>
2024-03-06 16:24:44 -0300

    Interpolação de String

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print('Sabor Express\n')
+print("""𝗦𝗮𝗯𝗼𝗿 𝗘𝘅𝗽𝗿𝗲𝘀𝘀\n""")
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
@@ -7,5 +7,4 @@ print('4. Sair\n')
 
 
 opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção', opcao_escolhida)
-
+print(f'Você escolheu a opção {opcao_escolhida}')
